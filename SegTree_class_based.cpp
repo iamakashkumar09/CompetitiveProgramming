3c82d22e8289ecfe1c1d8d8d6c1f6de31b80a4a0
Akash Kumar Gaud <142931881+iamakashkumar09@users.noreply.github.com>
2025-12-09 22:09:31 +0530

    Refactor Segment Tree for minimum queries

--- SegTree_class_based.cpp
+++ SegTree_class_based.cpp
@@ -1,6 +1,10 @@
+// Segment Tree Template using class based 
+// the problem solved in this example is finding the minimum of the index in the range
+
+
 #include <bits/stdc++.h>
 using namespace std;
-
+#define int long long 
 typedef long long ll;
 
 class SegmentTree {
@@ -17,18 +21,18 @@ private:
         int mid = (l + r) / 2;
         build(2*idx+1, l, mid);
         build(2*idx+2, mid+1, r);
-        tree[idx] = tree[2*idx+1] + tree[2*idx+2];
+        tree[idx] = min(tree[2*idx+1],tree[2*idx+2]);
     }
 
     ll query(int idx, int l, int r, int ql, int qr) {
-        if (ql > r || qr < l) return 0;
+        if (ql > r || qr < l) return LLONG_MAX;
         if (ql <= l && r <= qr) return tree[idx];
 
         int mid = (l + r) / 2;
         ll leftRes = query(2*idx+1, l, mid, ql, qr);
         ll rightRes = query(2*idx+2, mid+1, r, ql, qr);
 
-        return leftRes + rightRes;
+        return min(leftRes,rightRes);
     }
 
     void update(int idx, int l, int r, int pos, ll val) {
@@ -40,7 +44,7 @@ private:
         int mid = (l + r) / 2;
         if (pos <= mid) update(2*idx+1, l, mid, pos, val);
         else update(2*idx+2, mid+1, r, pos, val);
-        tree[idx] = tree[2*idx+1] + tree[2*idx+2];
+        tree[idx] = min(tree[2*idx+1],tree[2*idx+2]);
     }
 
 public:
@@ -51,7 +55,7 @@ public:
         build(0, 0, n-1);
     }
 
-    ll getSum(int l, int r) {
+    ll getMin(int l, int r) {
         return query(0, 0, n-1, l, r);
     }
 
@@ -59,3 +63,21 @@ public:
         update(0, 0, n-1, pos, val);
     }
 };
+
+signed main(){
+    int n,m; cin >> n >> m;
+    vector<int>arr(n);
+
+    for(int &e : arr) cin >> e;
+
+    SegmentTree st(arr);
+    
+    for(int i=0;i<m;i++){
+        int a,b,c; cin >> a >> b >> c;
+        if(a==1){
+            st.setValue(b,c);
+        }else{
+            cout << st.getMin(b,c-1) << endl;
+        }
+    }
+}
